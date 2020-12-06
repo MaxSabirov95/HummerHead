df0aa29cd3d386f03d6788e7e990da4c15c72d2e
Max Sabirov <maxim-sabirov@rambler.ru>
2020-12-06 20:59:07 +0200

    u5

--- HummerHead/Assets/Scripts/Cam.cs
+++ HummerHead/Assets/Scripts/Cam.cs
@@ -7,17 +7,23 @@ public class Cam : MonoBehaviour
 {
     public Transform cam;
     public static Cam camera;
+    public float followSpeed;
+    public float Y;
     //// Start is called before the first frame update
     void Start()
     {
         camera = this;
     }
 
-    //// Update is called once per frame
-    //void Update()
-    //{
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 CamPos = BlackBoard.player.transform.position;
+        CamPos.z = -10;
+        CamPos.y = Mathf.Max(CamPos.y,Y);
+        transform.position = Vector3.Lerp(transform.position, CamPos, followSpeed * Time.deltaTime);
+    }
 
-    //}
     public void ShakeCamera()
     {
         cam.DOShakePosition(0.25f);
